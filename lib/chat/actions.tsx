77ab1d0cbd6e831665c2a580e6ce9659d9ce051b
Amir Babi <125260479+ragaman1@users.noreply.github.com>
2024-09-02 18:28:13 +0000

    remove costum

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -8,7 +8,7 @@ import {
   streamUI,
   createStreamableValue
 } from 'ai/rsc'
-import { customAIStream } from './customAIClient'
+import { openai } from '@ai-sdk/openai'
 
 import { BotCard, BotMessage, SystemMessage } from '@/components/stocks'
 
